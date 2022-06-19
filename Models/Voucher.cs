9837c8ce20e081b3f9a39f11b9a36ce88325893b
Misha <inadequate.moron@outlook.com>
2022-06-19 15:53:02 +0300

    PredLastVersion

--- Models/Voucher.cs
+++ Models/Voucher.cs
@@ -11,6 +11,10 @@ namespace SmolenskTravelApi
         public int? Idclients { get; set; }
         public int? Idtours { get; set; }
         public DateTime? DateSale { get; set; }
+        public int? Amount { get; set; }
+        public int? NumberOrders { get; set; }
+        public string Comment { get; set; }
+        public int? Status { get; set; }
 
         public virtual Client IdclientsNavigation { get; set; }
         public virtual Tour IdtoursNavigation { get; set; }
